8610db33fbe11f4638ec948c393e1d509611bb85
George <georgina.carvell@gmail.com>
2019-07-23 21:56:40 +1200

    mask done

--- exp.py
+++ exp.py
@@ -20,8 +20,8 @@ trial = 0
 def open_window():
 	win = tk.Tk()
 	win.title('Sym Experiment')
-	win.geometry("700x700") #Setthe size of the app to be 500x500
-	win.resizable(0, 0) #Don't allow resizing in the x or y direction
+	win.attributes('-fullscreen', True)
+	win.bind('<Escape>',lambda e: win.destroy())
 	return win
 
 # Get text from file
@@ -52,10 +52,12 @@ def allow_click(allow):
 
 def hide_pattern():
 	grid.pack_forget()
+	mask.pack(fill='both', expand=True)
 	global wait
 	grid.after(wait, get_response)
 
 def get_response():
+	mask.pack_forget()
 	allow_click(True)
 	blank_grid()
 	grid.pack()
@@ -69,9 +71,9 @@ def submit():
 	grid.pack_forget()
 	submitBtn.pack_forget()
 	submitBtn.config(state="disabled")	
-	
+	# check!
 	feedback_cor.pack()
-	grid.after(1000, next_trial)
+	grid.after(2000, next_trial)
 	# log response
 	# pause then next trial
 def next_trial():
@@ -86,7 +88,7 @@ def next_trial():
 	if trial < len(patterns):
 		run_trial(patterns[trial])
 	else:
-		endText = tk.Label(win, text=display_text('end.txt'))
+		endText = tk.Label(win, text=display_text('end.txt'), justify=tk.CENTER, pady = 200, font=("Helvetica", 18))
 		endText.pack()
 
 # change colour of square on click
@@ -99,7 +101,7 @@ def swap_colour(loc):
 			loc=grid.find_withtag("current")[0]
 		else:
 			return
-		#get fill colour for canvas item rect
+		#get fill colour for circle
 		col = grid.itemcget(loc,"fill")
 		# if black, swap to white, vice versa
 		if col=="black":
@@ -129,15 +131,12 @@ def display_pattern(pattern):
 	for i in range(0, gridSize):
 		for j in range(0, gridSize):
 			k += 1
-			x1 = cellSize*(j+1)
-			x2 = x1 + cellSize
-			y1 = cellSize*(i+1)
-			y2 = y1 + cellSize
 			if pattern[i][j] == 0:
 				grid.itemconfig(k, fill = "white")
 			else:
 				grid.itemconfig(k, fill = "black")
 	global timer
+	# pattern is displayed for a specified duration (ms), then hidden
 	grid.after(timer, hide_pattern)
 
 def run_trial(pattern):
@@ -167,22 +166,17 @@ def start():
 	global trial
 	global patterns
 	run_trial(patterns[trial])
-	
 
-		# save subject responses
-		# check subject performance against correct pattern
-		# display feedback
+
 
 win = open_window()
 
 # Display start text
-txt = tk.Label(win, text=display_text('start.txt'))
+txt = tk.Label(win, text=display_text('start.txt'), justify=tk.CENTER, padx = 100, pady = 50, font=("Helvetica", 12))
 txt.pack()
 # Generate and display random ID number
 randID = r.randint(100,999)
-# rID = tk.Label(win, text = "Please record your ID number: {}".format(randID))
-# rID.pack()
-rID = tk.Label(win, text = "Time: {}".format(timer))
+rID = tk.Label(win, text = "Please record your ID number: {}".format(randID), pady = 10, font=("Helvetica", 12))
 rID.pack()
 # log randID
 
@@ -192,12 +186,17 @@ grid = draw_grid()
 # add START button
 startBtn = tk.Button(win, text="START", command=start)
 startBtn.pack()
+# prepare noise mask
+
+img = tk.PhotoImage(file='mask.gif')
+mask = tk.Label(win, image=img)
 
 # prepare submit button
 submitBtn = tk.Button(win, text = "Submit",command=submit)
 submitBtn.config(state="disabled")
 # prepare feedback
-feedback_cor = tk.Label(win, text=display_text('feedback_correct.txt'))
+feedback_cor = tk.Label(win, height = 200, width = 200, bg = 'green', text=display_text('feedback_correct.txt'), justify=tk.CENTER, pady = 200, font=("Helvetica", 50))
+feedback_incor = tk.Label(win, height = 200, width = 200, bg = 'green', text=display_text('feedback_incorrect.txt'), justify=tk.CENTER, pady = 200, font=("Helvetica", 50))
 
 try:
 	win.mainloop() #Open window
